64bd41436227cbc067edfaa570930c7f0c21b32d
Hristiana <xkl@abv.bg>
2020-07-10 12:18:19 +0300

    with dynamic colors

---
+++ js/change-color.js
@@ -0,0 +1,70 @@
+    /*Change a first circle color  */
+    $(document).ready(function() {
+        $(".dot").mouseover(function() {
+            $(".dot").css("background-color", "#7FFF00");
+        });
+        $(".dot").mouseout(function() {
+            $(".dot").css("background-color", "#0ae8b5");
+        });
+
+
+
+        /*Change a second circle color  */
+
+        $(".dot1").mouseover(function() {
+            $(".dot1").css("background-color", "#DC143C");
+        });
+        $(".dot1").mouseout(function() {
+            $(".dot1").css("background-color", "#fcbe78");
+        });
+
+
+        /*Change a third circle color  */
+
+        $(".dot2").mouseover(function() {
+            $(".dot2").css("background-color", "#9ACD32");
+        });
+        $(".dot2").mouseout(function() {
+            $(".dot2").css("background-color", "#ebffff");
+        });
+
+
+        /*Change a fourth circle color  */
+
+        $(".dot3").mouseover(function() {
+            $(".dot3").css("background-color", "#800080")
+        });
+        $(".dot3").mouseout(function() {
+            $(".dot3").css("background-color", "#f26941");
+        });
+
+
+        /*Change a fifth circle color  */
+
+        $(".dot4").mouseover(function() {
+            $(".dot4").css("background-color", "#66CDAA");
+        });
+        $(".dot4").mouseout(function() {
+            $(".dot4").css("background-color", "#78c1fa");
+        });
+
+
+        /*Change a sixth circle color  */
+
+        $(".dot5").mouseover(function() {
+            $(".dot5").css("background-color", "#9932CC");
+        });
+        $(".dot5").mouseout(function() {
+            $(".dot5").css("background-color", "#fc89f2");
+        });
+
+
+        /*Change a seventh circle color  */
+
+        $(".dot6").mouseover(function() {
+            $(".dot6").css("background-color", "#FFD700");
+        });
+        $(".dot6").mouseout(function() {
+            $(".dot6").css("background-color", "#456bd9");
+        });
+    });
